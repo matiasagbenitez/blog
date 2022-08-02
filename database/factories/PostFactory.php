14aa86e5c467e100814a94569440513b540b5c83
Matías <matiasagbenitez@gmail.com>
2022-08-02 14:42:31 -0300

    Factories and Seeders were configured to fill the database with test data

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class PostFactory extends Factory
+{
+    public function definition()
+    {
+        $name = $this->faker->unique()->sentence();
+
+        return [
+            'name' => $name,
+            'slug' => Str::slug($name),
+            'extract' => $this->faker->text(200),
+            'body' => $this->faker->text(1000),
+            'status' => $this->faker->randomElement([1, 2]),
+            'category_id' => Category::all()->random()->id,
+            'user_id' => User::all()->random()->id
+        ];
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::create([
+            'name' => 'Matías Benítez',
+            'email' => 'matias@correo.com',
+            'password' => bcrypt('password')
+        ]);
+
+        User::factory(9)->create();
+    }
+}
