fee19c45d00bcc8a3aeeeb7b6e9e64a9818d435d
Matías <matiasagbenitez@gmail.com>
2022-08-03 10:23:04 -0300

    Functionality to LIST categories added

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('admin.home');
+
+Route::resource('categories', CategoryController::class)->names('admin.categories');
